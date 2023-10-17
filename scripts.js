cadf3918a0edc2c8161fea1417bd411887b22b57
rkanishka <143039356+rkanishka@users.noreply.github.com>
2023-10-17 13:08:43 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
-alert("HELLO WORLD");
-var favfood='gol gappe';
-document.write(favfood);
+
+var  Pokemonlist = [];
+let PokemonList={
+
+    
+         name:'BALBASAUR',
+      height:0.7,
+      type:['GRASS','POISON'],
+         name:'GOLBAT',      
+      height:1.6, 
+      type:'FLYING',
+          name:'MEOWTH',
+        height:0.4,
+        type:'NORMAL',
+           name:'RAICHU',
+       height:0.8,
+       type:'ELECTRIC',
+           name:'NIDORINA',
+      height:0.8,
+      type:'POISON',
+               name:'MACHOKE', 
+              height:1.5, 
+              type:'FIGHTING',
+};
+console.log(PokemonList);
+document.write(PokemonList);
+
+
