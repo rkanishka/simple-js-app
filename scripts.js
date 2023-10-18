0947745492a22d17132e649a0f047f7db4f0f5b5
rkanishka <143039356+rkanishka@users.noreply.github.com>
2023-10-17 20:44:22 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,28 +1,44 @@
-
-var  Pokemonlist = [];
-let PokemonList={
+let pokemonlist=[];
+let pokemon1={  
+       name:'BALBASAUR',
+       height:0.7,
+       type:['GRASS','POISON']};
+let pokemon2={ 
+      name:'GOLBAT',      
+      height:1.6, 
+      type:'FLYING'};
+let pokemon3={
+      name:'MEOWTH',
+      height:0.4,
+      type:'NORMAL'};
+let pokemon4={     
+      name:'RAICHU',
+      height:0.8,
+      type:'ELECTRIC'};
+let pokemon5={      
+      name:'NIDORINA',
+      height:0.8,
+      type:'POISON'};
+let pokemon6={
+      name:'MACHOKE', 
+     height:1.5, 
+     type:'FIGHTING'
+};
+pokemonlist[0]=pokemon1;
+pokemonlist[1]=pokemon2;
+pokemonlist[2]=pokemon3;
+pokemonlist[3]=pokemon4;
+pokemonlist[4]=pokemon5;
+pokemonlist[5]=pokemon6;
+    console.log(pokemonlist);
+    console.log(pokemonlist[0]);
+    console.log(pokemonlist[0].type);
+    console.log(pokemonlist[1]);
+    console.log(pokemonlist[2]);
+    console.log(pokemonlist[3]);
+    console.log(pokemonlist[4]);
+    console.log(pokemonlist[5]);
 
     
-         name:'BALBASAUR',
-      height:0.7,
-      type:['GRASS','POISON'],
-         name:'GOLBAT',      
-      height:1.6, 
-      type:'FLYING',
-          name:'MEOWTH',
-        height:0.4,
-        type:'NORMAL',
-           name:'RAICHU',
-       height:0.8,
-       type:'ELECTRIC',
-           name:'NIDORINA',
-      height:0.8,
-      type:'POISON',
-               name:'MACHOKE', 
-              height:1.5, 
-              type:'FIGHTING',
-};
-console.log(PokemonList);
-document.write(PokemonList);
-
-
+      
+     
