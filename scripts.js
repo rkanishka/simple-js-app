9181c54642219ab4171c3fee61117599eae8efc9
rkanishka <143039356+rkanishka@users.noreply.github.com>
2023-10-20 11:38:26 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,37 +2,40 @@ let pokemonlist=[
 {  
        name:'BALBASAUR',
        height:0.7,
-       type:['GRASS','POISON']},
+       type:['GRASS','POISON']
+},
 { 
       name:'GOLBAT',      
       height:1.6, 
-      type:['FLYING']},
+      type:['FLYING']
+},
 {
       name:'MEOWTH',
       height:0.4,
-      type:['NORMAL']},
+      type:['NORMAL']
+},
 {     
       name:'RAICHU',
       height:0.8,
-      type:['ELECTRIC']},
+      type:['ELECTRIC']
+},
 {      
       name:'NIDORINA',
       height:0.8,
-      type:['POISON']},
+      type:['POISON']
+},
 {
       name:'MACHOKE', 
      height:1.5, 
      type:['FIGHTING']
 }];
-
-    console.log(pokemonlist);
-    console.log(pokemonlist[0]);
-    console.log(pokemonlist[0].type);
-    console.log(pokemonlist[1]);
-    console.log(pokemonlist[2]);
-    console.log(pokemonlist[3]);
-    console.log(pokemonlist[4]);
-    console.log(pokemonlist[5]);
+for( let i=0; i< pokemonlist.length; i++)
+{ if (pokemonlist[i].height>1.0)
+      {
+            console.log(pokemonlist[i]+"wow that's big");
+      }else{
+      console.log(pokemonlist[i]);}
+      }
 
 
       
